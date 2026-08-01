312b94a3bbd1e1f0072fae8189e4a3fcdd7adaeb
Thales Oliveira <TOliveiraDev@outlook.com>
2026-08-01 18:56:36 -0300

    feat: adiciona componentes da homepage (mobile-first)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './shared/components/header/header';
+import { Footer } from './shared/components/footer/footer';
+import { MatDividerModule } from '@angular/material/divider';
+import { MainPage } from './shared/components/main-page/main-page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Header,
+    MainPage,
+    MatDividerModule,
+    Footer,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
