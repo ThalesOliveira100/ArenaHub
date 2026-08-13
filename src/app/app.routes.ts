8ca2d1325b9ab8d2acd294ac7fc5178e648bd532
Thales Oliveira <TOliveiraDev@outlook.com>
2026-08-12 23:30:57 -0300

    feat: define initial application routes for homepage, login, and quadras

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Auth } from './pages/auth/auth';
+import { Homepage } from './pages/homepage/homepage';
+import { Quadras } from './pages/quadras/quadras';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", component: Homepage},
+  { path: "login", component: Auth },
+  { path: "quadras", component: Quadras},
+  // { path: "quadras/:id"}
+];
