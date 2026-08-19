0c32538634e2683eb64f9c738b618e774fcffc02
Thales Oliveira <TOliveiraDev@outlook.com>
2026-08-19 02:55:55 -0300

    fix: update listaDeHorarios to use input binding and correct loop syntax in QuadraGradeHorarios component

--- src/app/pages/quadras/quadra-detalhes/quadra-grade-horarios/quadra-grade-horarios.ts
+++ src/app/pages/quadras/quadra-detalhes/quadra-grade-horarios/quadra-grade-horarios.ts
@@ -12,33 +12,5 @@ import { MatDivider } from "@angular/material/divider";
 })
 export class QuadraGradeHorarios {
   quadra = input.required<Quadra | undefined>();
-  listaDeHorarios: GradeHorario[] = [
-    {
-      id: 0,
-      quadraId: 1,
-      diaDaSemana: 1,
-      horaInicio: '18:00',
-      horaFinal: '19:30',
-      esporte: 'Futsal',
-      solicitanteNome: 'Liga Centro'
-    },
-    {
-      id: 1,
-      quadraId: 1,
-      diaDaSemana: 1,
-      horaInicio: '19:30',
-      horaFinal: '21:00',
-      esporte: 'Vôlei',
-      solicitanteNome: 'Thales'
-    },
-    {
-      id: 2,
-      quadraId: 1,
-      diaDaSemana: 2,
-      horaInicio: '08:00',
-      horaFinal: '10:00',
-      esporte: 'Vôlei',
-      solicitanteNome: 'Vôlei NeoPL'
-    }
-  ]
+  listaDeHorarios = input<GradeHorario[]>([]);
 }
