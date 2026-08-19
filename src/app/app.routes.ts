88750f20b83795bc43862b931326852655c08828
Thales Oliveira <TOliveiraDev@outlook.com>
2026-08-19 15:43:20 -0300

    fix: update routing to use loadComponent for lazy loading and add perfilGuard for dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,18 @@
 import { Routes } from '@angular/router';
-import { Auth } from './pages/auth/auth';
-import { Homepage } from './pages/homepage/homepage';
-import { Quadras } from './pages/quadras/quadras';
-import { QuadraDetalhes } from './pages/quadras/quadra-detalhes/quadra-detalhes';
+import { Perfil } from './core/models/perfil.model';
+import { perfilGuard } from './core/auth/perfil.guard';
 
 export const routes: Routes = [
-  { path: "", component: Homepage},
-  { path: "login", component: Auth },
-  { path: "quadras", component: Quadras},
-  { path: "quadras/:id", component: QuadraDetalhes}
+  // Rotas Públicas
+  { path: "", loadComponent: () => import('@features/publico/home/home').then(m => m.Home) },
+  { path: "login", loadComponent: () => import('@features/autenticacao/auth').then(m => m.Auth) },
+  { path: "quadras", loadComponent: () => import('@features/publico/quadras/quadras').then(m => m.Quadras) },
+  { path: "quadras/:id", loadComponent: () => import('@features/publico/quadras/quadra-detalhes/quadra-detalhes').then(m => m.QuadraDetalhes) },
+
+  // Rotas Privadas
+  {
+    path: 'dashboard',
+    loadComponent: () => import('@features/dashboard/dashboard').then(m => m.Dashboard),
+    canActivate: [perfilGuard]
+  },
 ];
