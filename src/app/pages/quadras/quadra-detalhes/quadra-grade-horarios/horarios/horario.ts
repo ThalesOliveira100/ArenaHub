7349745065f4c4f828548ffeaf624c6374094b21
Thales Oliveira <TOliveiraDev@outlook.com>
2026-08-19 02:55:35 -0300

    fix: correct data binding and import DiaSemanaPipe in Horario component

--- src/app/pages/quadras/quadra-detalhes/quadra-grade-horarios/horarios/horario.ts
+++ src/app/pages/quadras/quadra-detalhes/quadra-grade-horarios/horarios/horario.ts
@@ -1,12 +1,15 @@
 import { Component, input } from '@angular/core';
 import { GradeHorario } from '../../../../../core/models/grade-horario.model';
+import { DiaSemanaPipe } from '../../../../../shared/pipes/dia-semana.pipe';
 
 @Component({
   selector: 'app-horario',
-  imports: [],
+  imports: [
+    DiaSemanaPipe
+  ],
   templateUrl: './horario.html',
   styleUrl: './horario.scss',
 })
 export class Horario {
-  dados = input.required<GradeHorario | undefined>();
+  dados = input.required<GradeHorario>();
 }
