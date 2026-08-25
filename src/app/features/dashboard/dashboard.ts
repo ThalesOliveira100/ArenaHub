c328740f6d490bfc78680573b7a1cab0f65d4cd0
Thales Oliveira <TOliveiraDev@outlook.com>
2026-08-25 19:07:26 -0300

    feat: enhance dashboard with user-specific data handling and new chart components

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,3 +1,5 @@
+import { RelatoriosConsumoService } from './../../core/services/relatorios-consumo-service';
+import { QuadrasService } from '@core/services/quadras-service';
 import { Component, computed, inject } from '@angular/core';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
@@ -7,13 +9,16 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { Logo } from '@shared/components/logo/logo';
 import { DashboardHeader } from "@shared/components/dashboard-components/dashboard-header/dashboard-header";
-import { AutenticacaoService } from '../../core/auth/autenticacao.service';
+import { AutenticacaoService } from '@core/auth/autenticacao.service';
 import { Router } from '@angular/router';
 import { Footer } from "@shared/components/footer/footer";
 import { DashboardCard } from '@shared/components/dashboard-components/dashboard-card/dashboard-card';
 import { BreakpointObserver } from '@angular/cdk/layout';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { map } from 'rxjs';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { forkJoin, map, of, switchMap } from 'rxjs';
+import { DashboardGraficoHorasEsporte } from "@shared/components/dashboard-components/dashboard-grafico-horas-esporte/dashboard-grafico-horas-esporte";
+import { GradeHorarioService } from '@core/services/grade-horario-service';
+import { DashboardGraficoConsumoMensalOcupacao } from "@shared/components/dashboard-components/dashboard-grafico-consumo-mensal-ocupacao/dashboard-grafico-consumo-mensal-ocupacao";
 
 const MODULES = [
   MatIcon,
@@ -29,7 +34,9 @@ const COMPONENTS = [
     Logo,
     DashboardHeader,
     Footer,
-    DashboardCard
+    DashboardCard,
+    DashboardGraficoHorasEsporte,
+    DashboardGraficoConsumoMensalOcupacao,
 ]
 
 @Component({
@@ -42,7 +49,12 @@ const COMPONENTS = [
   styleUrl: './dashboard.scss',
 })
 export class Dashboard {
+  // SERVICES
   private authService = inject(AutenticacaoService);
+  private quadrasService = inject(QuadrasService);
+  private gradesService = inject(GradeHorarioService);
+  private relatoriosConsumoService = inject(RelatoriosConsumoService);
+
   private router = inject(Router);
   private breakpointObserver = inject(BreakpointObserver);
 
@@ -56,12 +68,74 @@ export class Dashboard {
   protected readonly sidenavMode = computed(() => this.isDesktop() ? 'side' : 'over');
   protected readonly sidenavOpened = computed(() => this.isDesktop());
 
-  usuarioLogado = this.authService.usuarioLogado();
+  protected readonly usuarioLogado = this.authService.usuarioLogado;
 
-  cardsVisiveis = [
+  protected readonly quadrasPorUsuario = toSignal(
+    toObservable(this.usuarioLogado).pipe(
+      switchMap(usuario => {
+        if (!usuario || !usuario?.id) return of([]);
+        if (usuario.perfil === 'MONITOR') return this.quadrasService.getQuadrasByMonitor(usuario.id);
+        if (usuario.perfil === 'GESTOR') return this.quadrasService.getQuadrasByGestor(usuario.id);
+        if (usuario.perfil === 'ADMIN') return this.quadrasService.getQuadras();
+
+        return of([]);
+      })
+    ),
+    { initialValue: [] }
+  );
+
+  protected readonly gradesDoUsuario = toSignal(
+    toObservable(this.usuarioLogado).pipe(
+      switchMap(usuario => {
+        if (!usuario || !usuario.id) {
+          return of([]);
+        };
+
+        if (usuario.perfil === 'ADMIN') {
+          return this.gradesService.getGradeHorarios();
+        }
+
+        const quadras = this.quadrasPorUsuario();
+        if (quadras.length === 0) {
+          return of([]);
+        }
+
+        const ids = quadras.map(q => q.id);
+        return this.quadrasService.getGradesHorariosByQuadras(ids);
+      })
+    ),
+    { initialValue: [] }
+  );
+
+  protected readonly dadosConsumoDoUsuario = toSignal(
+    toObservable(this.quadrasPorUsuario).pipe(
+      switchMap(quadras => {
+        const usuario = this.usuarioLogado();
+
+        if (!quadras || quadras.length === 0 || !usuario || usuario.perfil === 'PUBLICO') return of([]);
+
+        if (usuario.perfil === 'ADMIN') {
+          return this.relatoriosConsumoService.getDadosConsumo();
+        }
+
+        const requests = quadras.map(q => this.relatoriosConsumoService.getDadosConsumo(String(q.id)));
+
+        return forkJoin(requests).pipe(
+          map(resultados => resultados.flat())
+        )
+      })
+    ),
+    { initialValue: [] }
+  );
+
+  protected readonly dadosConsumoMensalOcupacaoQuadra = computed(() => {
+    return this.dadosConsumoDoUsuario();
+  });
+
+  protected readonly cardsVisiveis = computed(() => [
     {
       "title": "Quadras",
-      "value": 4,
+      "value": this.quadrasPorUsuario().length,
       "subtitle": "sob gestão",
       "icon": "pin_drop"
     },
@@ -89,10 +163,10 @@ export class Dashboard {
       "subtitle": "R$ 350",
       "icon": "description"
     },
-  ]
+  ])
 
-  onSideBarButtonClick() {
-
+  onSideBarButtonClick():void {
+    console.log(this.quadrasPorUsuario());
   }
 
   onLogout(): void {
