e71eacf14d520a22fc7bcd227e75e3c17ad856ab
Thales Oliveira <TOliveiraDev@outlook.com>
2026-08-24 21:13:19 -0300

    feat: add getGradesHorariosByQuadras method to QuadrasService for fetching schedule grades

--- src/app/core/services/quadras-service.ts
+++ src/app/core/services/quadras-service.ts
@@ -1,9 +1,10 @@
 import { computed, inject, Injectable, Signal, signal } from '@angular/core';
 import { environment } from '../../../environments/environments.prod';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Quadra } from '../models/quadra.model';
 import { SolicitacaoHorario } from '../models/solicitação-horario.model';
+import { GradeHorario } from '@core/models/grade-horario.model';
 
 @Injectable({
   providedIn: 'root',
@@ -27,6 +28,16 @@ export class QuadrasService {
 
   getQuadrasByGestor(gestorId: number) {
     return this.http.get<Quadra[]>(`${this.apiUrl}/quadras/?gestorId=${gestorId}`);
+  }
+
+  getGradesHorariosByQuadras(quadraIds: number[]): Observable<GradeHorario[]> {
+    if (!quadraIds || quadraIds.length === 0) {
+      return of([]);
+    }
+
+    const query = quadraIds.map(id => `quadraId=${id}`).join('&');
+
+    return this.http.get<GradeHorario[]>(`${this.apiUrl}/grade_horarios?${query}`);
   }
 
   sendSolicitacaoHorario(solicitacao: Omit<SolicitacaoHorario, 'id'>): Observable<SolicitacaoHorario> {
