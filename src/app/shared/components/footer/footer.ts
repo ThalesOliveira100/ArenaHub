7f50a43d427a899bbf3dee42e4d9c0ba9b7fa27f
Thales Oliveira <TOliveiraDev@outlook.com>
2026-08-27 10:45:52 -0300

    feat: implement dynamic footer links based on user authentication status

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AutenticacaoService } from '@core/auth/autenticacao.service';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
-export class Footer {}
+export class Footer {
+  private authService = inject(AutenticacaoService);
+
+  protected readonly footerConfig = computed(() => {
+    const logado = !!this.authService.usuarioLogado();
+
+    return logado
+      ? { text: 'Ver portal público', link: '/' }
+      : { text: 'Área Restrita', link: '/login' };
+  });
+}
