e55e9b21236d65376fe6bdd6b50dd55f16c5cf8a
Thales Oliveira <TOliveiraDev@outlook.com>
2026-08-30 12:59:34 -0300

    feat: update header component to use RouterLink for navigation and dynamic login link

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,19 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { Logo } from "../logo/logo";
+import { AutenticacaoService } from '@core/auth/autenticacao.service';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-header',
   imports: [
     MatButtonModule,
     MatToolbarModule,
-    Logo
+    Logo,
+    RouterLink
 ],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
+  private authService = inject(AutenticacaoService);
+
+  protected readonly headerConfig = computed(() => {
+    const logado = !!this.authService.usuarioLogado();
+
+    return logado
+      ? { text: 'Ir para o painel', link: '/dashboard' }
+      : { text: 'Entrar', link: '/login' };
+  });
+
 
   protected direcionarParaLogin() {
 
