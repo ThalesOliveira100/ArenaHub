23420293b0a2deba0806f52e10fc78093e8815b6
Thales Oliveira <TOliveiraDev@outlook.com>
2026-09-09 14:10:36 -0300

    refactor: refatora main-page para utilizar sinais computados puxados do componente de state

--- src/app/shared/components/main-page/main-page.ts
+++ src/app/shared/components/main-page/main-page.ts
@@ -1,11 +1,10 @@
-import { Component, computed, inject } from '@angular/core';
+import { DashboardStateService } from './../../../core/services/dashboard-state-service';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { BasicCard } from '../basic-card/basic-card';
 import { RouterLink } from '@angular/router';
-import { QuadrasService } from '@core/services/quadras-service';
-import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-main-page',
@@ -20,20 +19,10 @@ import { toSignal } from '@angular/core/rxjs-interop';
   styleUrl: './main-page.scss',
 })
 export class MainPage {
-  private quadrasService = inject(QuadrasService);
+  private stateService = inject(DashboardStateService);
 
-  protected readonly quadras = toSignal(
-    this.quadrasService.getQuadras(),
-    { initialValue: [] }
-  );
-
-  protected getQuantQuadrasCadastradas = computed(() => {
-    const lista = this.quadras();
-    return lista ? lista.length : 0
-  });
-
-  protected readonly quantQuadrasAtivas: number = 3;
-  protected readonly quantEventosAgendados: number = 3;
-  protected readonly quantPerfisDeAcesso: number = 67;
-
+  protected readonly quadras = this.stateService.todasAsQuadras;
+  protected readonly quadrasAtivas = this.stateService.quadrasAtivas;
+  protected readonly usuarios = this.stateService.todosOsUsuarios;
+  protected readonly totalEventos = this.stateService.quantidadeEventosFuturos;
 }
