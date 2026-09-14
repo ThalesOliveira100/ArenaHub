4f596572d364e3d720fe31430e28348762793ba7
Thales Oliveira <TOliveiraDev@outlook.com>
2026-09-14 18:51:03 -0300

    feat: implement search bar and enhance dashboard patrimonio with state management and header definition

--- src/app/features/dashboard/pages/dashboard-patrimonio/dashboard-patrimonio.ts
+++ src/app/features/dashboard/pages/dashboard-patrimonio/dashboard-patrimonio.ts
@@ -1,9 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, inject } from '@angular/core';
+import { DashboardStateService } from '@core/services/dashboard-state-service';
+import { HeaderService } from '@core/services/header/header-service';
+import { SearchBar } from '@shared/components/search-bar/search-bar';
 
 @Component({
   selector: 'app-dashboard-patrimonio',
-  imports: [],
+  imports: [SearchBar],
   templateUrl: './dashboard-patrimonio.html',
   styleUrl: './dashboard-patrimonio.scss',
 })
-export class DashboardPatrimonio {}
+export class DashboardPatrimonio {
+  private headerService = inject(HeaderService);
+  private dashboardState = inject(DashboardStateService);
+
+  protected readonly usuarioLogado = this.dashboardState.usuarioLogado;
+
+  protected readonly permissoes = computed(() => {
+    const perfil = this.usuarioLogado()?.perfil;
+
+    return {
+      criar: perfil === 'ADMIN',
+      editar: perfil === 'ADMIN' || perfil === 'GESTOR',
+      excluir: perfil === 'ADMIN',
+      visualizar: perfil === 'ADMIN' || perfil === 'GESTOR' || perfil === 'MONITOR'
+    };
+  });
+
+  constructor() {
+    effect(() => {
+      const podeCriar = this.permissoes().criar;
+
+      this.headerService.definirCabecalho(
+        'Patrimônio',
+        'Patrimônio',
+        'Redes, traves, placares, iluminação e demais ativos imobilizados.',
+        podeCriar ? {
+          label: 'Novo ativo',
+          icon: 'add',
+          color: 'primary',
+          action: () => this.abrirModalCriar()
+        } : undefined
+      );
+    });
+  };
+
+  abrirModalCriar() { console.log(`Abrir modal de criação`)}
+}
