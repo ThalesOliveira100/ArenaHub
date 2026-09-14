596dd8ffa7d0f398ab352ba4735db05e58a8622b
Thales Oliveira <TOliveiraDev@outlook.com>
2026-09-14 18:50:50 -0300

    feat: implement search bar and enhance dashboard eventos component with state management and permissions

--- src/app/features/dashboard/pages/dashboard-eventos/dashboard-eventos.ts
+++ src/app/features/dashboard/pages/dashboard-eventos/dashboard-eventos.ts
@@ -1,9 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, inject } from '@angular/core';
+import { DashboardStateService } from '@core/services/dashboard-state-service';
+import { HeaderService } from '@core/services/header/header-service';
+import { SearchBar } from '@shared/components/search-bar/search-bar';
 
 @Component({
   selector: 'app-dashboard-eventos',
-  imports: [],
+  imports: [SearchBar],
   templateUrl: './dashboard-eventos.html',
   styleUrl: './dashboard-eventos.scss',
 })
-export class DashboardEventos {}
+export class DashboardEventos {
+  private headerService = inject(HeaderService);
+  private dashboardState = inject(DashboardStateService);
+
+  protected readonly usuarioLogado = this.dashboardState.usuarioLogado;
+
+  protected readonly permissoes = computed(() => {
+    const perfil = this.usuarioLogado()?.perfil;
+
+    return {
+      criar: perfil === 'ADMIN',
+      editar: perfil === 'ADMIN' || perfil === 'GESTOR',
+      excluir: perfil === 'ADMIN',
+      visualizar: perfil === 'ADMIN' || perfil === 'GESTOR' || perfil === 'MONITOR'
+    };
+  });
+
+  constructor() {
+    effect(() => {
+      const podeCriar = this.permissoes().criar;
+
+      this.headerService.definirCabecalho(
+        'Eventos',
+        'Eventos',
+        'Competições, festivais e atividades programadas nas quadras.',
+        podeCriar ? {
+          label: 'Novo evento',
+          icon: 'add',
+          color: 'primary',
+          action: () => this.abrirModalCriar()
+        } : undefined
+      );
+    });
+  };
+
+  abrirModalCriar() { console.log(`Abrir modal de criação`)}
+}
