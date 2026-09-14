eb7a0eb6a9a3032222c89e607b3f3189b103f03b
Thales Oliveira <TOliveiraDev@outlook.com>
2026-09-14 18:50:56 -0300

    feat: add search bar component and implement header management in dashboard horarios

--- src/app/features/dashboard/pages/dashboard-horarios/dashboard-horarios.ts
+++ src/app/features/dashboard/pages/dashboard-horarios/dashboard-horarios.ts
@@ -1,9 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, inject } from '@angular/core';
+import { DashboardStateService } from '@core/services/dashboard-state-service';
+import { HeaderService } from '@core/services/header/header-service';
+import { SearchBar } from '@shared/components/search-bar/search-bar';
 
 @Component({
   selector: 'app-dashboard-horarios',
-  imports: [],
+  imports: [SearchBar, ],
   templateUrl: './dashboard-horarios.html',
   styleUrl: './dashboard-horarios.scss',
 })
-export class DashboardHorarios {}
+export class DashboardHorarios {
+  private headerService = inject(HeaderService);
+  private dashboardState = inject(DashboardStateService);
+
+  protected readonly usuarioLogado = this.dashboardState.usuarioLogado;
+
+  protected readonly permissoes = computed(() => {
+    const perfil = this.usuarioLogado()?.perfil;
+
+    return {
+      criar: perfil === 'ADMIN',
+      editar: perfil === 'ADMIN' || perfil === 'GESTOR',
+      excluir: perfil === 'ADMIN',
+      visualizar: perfil === 'ADMIN' || perfil === 'GESTOR' || perfil === 'MONITOR'
+    };
+  });
+
+  constructor() {
+    effect(() => {
+      const podeCriar = this.permissoes().criar;
+
+      this.headerService.definirCabecalho(
+        'Horários',
+        'Horários',
+        'Grade semanal de utilização das quadras.',
+        podeCriar ? {
+          label: 'Novo horário',
+          icon: 'add',
+          color: 'primary',
+          action: () => this.abrirModalCriar()
+        } : undefined
+      );
+    });
+  };
+
+  abrirModalCriar() { console.log(`Abrir modal de criação`)}
+}
